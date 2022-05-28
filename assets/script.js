7f657ca0ad2c62c69ea83861d3aaaeefcd710efa
panecatl <ypanecatl24@gmail.com>
2022-05-28 17:16:40 -0500

    localStorage setItem

--- assets/script.js
+++ assets/script.js
@@ -138,10 +138,11 @@ var initial = document.querySelector("contactInfo");
 
 userInfo.addEventListener("click", function(event) {
     var user = {
-        userName: initial.ariaValueMax.trim()
+        userName: contactInfo.value.trim()
     };
 
     localStorage.setItem("user", JSON.stringify(user));
-    localStorage.getItem("user", JSON.stringify(user));
+
+    localStorage.getItem("user", JSON.parse(user));
 });
 
